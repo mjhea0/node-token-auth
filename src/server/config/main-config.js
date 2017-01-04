94b8d673021c0e5ad0b2948b378b54388be37140
Michael Herman <hermanmu@gmail.com>
2017-01-03 19:47:29 -0700

    added access origin

--- src/server/config/main-config.js
+++ src/server/config/main-config.js
@@ -21,6 +21,16 @@
     if (process.env.NODE_ENV !== 'test') {
       app.use(morgan('dev'));
     }
+
+    // *** cross domain requests *** //
+    const allowCrossDomain = function(req, res, next) {
+      res.header('Access-Control-Allow-Origin', '*');
+      res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+      res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+      next();
+    };
+
+    app.use(allowCrossDomain);
     app.use(cookieParser());
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: false }));
